5deab4f13e52a22711aa796b8b9bad796223875a
Sabavath Sravan <128413775+sabavathsravan1999@users.noreply.github.com>
2025-10-12 20:52:51 +0530

    Create capgeminiCompA.js

---
+++ DAY4/LWC_DAY4/byUsingLMS/capgeminiCompA.js
@@ -0,0 +1,89 @@
+import { LightningElement, wire } from 'lwc';
+import getaccCon from '@salesforce/apex/getAccConforCapgeminiINterview.getaccCon';
+import { publish, MessageContext } from 'lightning/messageService';
+import MESSGAE_CHANNEL from '@salesforce/messageChannel/capgeminidatatransfer__c';
+/*const action=[
+    {label:'View', name:'view'},
+    {label:'Edit', name:'edit'},
+    {label:'Delete', name:'delete'}
+
+];*/
+const COLUMNS = [
+    { label: 'Account Name', fieldName: 'Name' },
+    { label: 'Account Phone', fieldName: 'Phone' },
+    {
+        label: 'Contact', type: 'button',
+        typeAttributes: {
+            label: 'Contact',
+            name: 'contact',
+            variant: 'brand'
+        }
+    }
+];
+export default class CapgeminiCompA extends LightningElement {
+    @wire(MessageContext)
+    messageContext;
+
+    columns = COLUMNS;
+    account;
+    contacts = null;
+    @wire(getaccCon)
+    allaccCon({ error, data }) {
+        if (data) {
+            this.account = data;
+            console.log('data of', data);
+        } else if (error) {
+            console.log(error);
+        }
+    }
+
+    onrwoHandelr(event) {
+        const action_name = event.detail.action.name;
+        //console.log(action_name);
+        const row_detail = event.detail.row;
+        if (action_name === 'contact') {
+            if (row_detail.Contacts.length > 0) {
+                this.contacts = row_detail.Contacts;
+                console.log('account data ', row_detail);
+                console.log('account Name ', row_detail.Name);
+                //by using lms model lightning message channel
+                const payload = {
+                    accountName: row_detail.Name,
+                    contactsofChannel: this.contacts
+                };
+                publish(this.messageContext, MESSGAE_CHANNEL, payload);
+                this.contacts = row_detail.Contacts;
+                console.log('contact row ->', this.contacts);
+            }
+        }
+    }
+
+    /* handlerowActions(event){
+         const action_name=event.detail.action.name; // tells whataction is clied like view, edit, delete 
+         console.log('what is the action anme ', action_name);
+         const row_detail=event.detail.row; //tells about the data inside the seleted row
+         console.log('tells about the row data ', row_detail);
+         console.log('tells about the row of contact data  ', row_detail.Contacts);
+         switch (action_name){
+             case 'view':
+                 console.log('action name is -->', action_name);
+                 //perform actions
+                 break;
+             case 'edit':
+                 console.log('action name is -->', action_name);
+                 //perform actions
+ 
+                 break;
+             case 'delete':
+                 console.log('action name is -->', action_name);
+                 //perform actions
+ 
+                 break;
+             default:
+                 console.log('Unknown action:', action_name);
+         }
+ 
+ 
+     }*/
+
+}
