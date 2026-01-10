8851ed8e0b7110aeb62607296f7850504cf71c72
Sabavath Sravan <128413775+sabavathsravan1999@users.noreply.github.com>
2026-01-11 02:34:56 +0530

    Create generatePDFsendEmailExm1.js

---
+++ DAY5/LWC_DAY5/generatePDFsendEmailExm1.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+import getpdf from '@salesforce/apex/GeneratepgfControlerExp1.getpdf';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+export default class GeneratePDFsendEmailExm1 extends LightningElement {
+    conname='';
+	Phone='';
+	Email='';
+	handlechange(event){
+		this[event.target.name]=event.target.value;
+	}
+    handleclick(event){
+        getpdf({name:this.conname, Phone:this.Phone, Email:this.Email})
+        .then(result => {
+            const ev=new ShowToastEvent({
+                title:'Data Pdf send to Email',
+                message:result.Id+' inserted and pdf sent to email',
+                variant:'success',
+                mode:'dismissable'
+            });
+            this.dispatchEvent(ev);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+}
+}
